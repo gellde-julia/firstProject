f4d7908042058d4fd801e4533069e9b624f5ce7d
Gellde <gelldesmodus@gmail.com>
2022-07-06 18:34:44 +0300

    modified

--- script.js
+++ script.js
@@ -78,13 +78,25 @@ let appData = {
 
     chooseIncome: function() {
         let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
-        appData.income = items.split(", ");
-        appData.income.push(prompt("Что-то еще?"));
-        appData.income.sort();
+        if (typeof(items) != "string" || items == "" || typeof(items) == null) {
+            console.log("Вы ввели некорректные данные или не ввели их вовсе");
+        } else {
+            appData.income = items.split(", ");
+            appData.income.push(prompt("Может что-то еще?"));
+            appData.income.sort();
+        }
+
+        appData.income.forEach (function (itemmassive, i) {
+            alert("Способы доп. заработка: " + (i+1) + " - " + itemmassive);
+        });
 
     } 
 
 };
+
+for (let key in appData) {
+    console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
+}
 
 
 
